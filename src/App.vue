cc8c619e39d5db97a4e48bd4a669eb04f8e48dd0
tgd <tgd1106@163.com>
2019-12-10 18:31:42 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="app">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+    <div class="siderbar">siderbar</div>
+    <div class="container">
+      <div class="header">header</div>
+      <div class="content">content</div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.app {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  .siderbar {
+    width: 240px;
+  }
+  .container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    .header {
+      flex: none;
+      height: 50px;
+      line-height: 50px;
+    }
+    .content {
+      flex: 1;
     }
   }
 }
