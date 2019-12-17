e0db745b035287640e6b32e964f4a0aad53716c6
tgd <tgd1106@163.com>
2019-12-17 10:46:04 +0800

    多个marker带popup

--- src/views/marker/markers/index.vue
+++ src/views/marker/markers/index.vue
@@ -11,13 +11,41 @@ import 'leaflet/dist/leaflet.css'
 // 定位中心点
 const center = [30.659791980587134, 104.0633797645569]
 
+const monitorImg = require('@/assets/gis/monitor.png')
+const monitorActiveImg = require('@/assets/gis/monitor-active.png')
+function produceIcon (iconUrl) {
+  return L.Icon.extend({
+    options: {
+      iconUrl: iconUrl,
+      iconSize: [36, 39],
+      iconAnchor: [18, -5]
+    }
+  })
+}
+const Icon = produceIcon(monitorImg)
+const IconActive = produceIcon(monitorActiveImg)
+// marker正常态
+const icon = new Icon()
+// marker点击态
+const iconActive = new IconActive()
+
 export default {
+  data () {
+    return {
+      map: null,
+      curMarker: null
+    }
+  },
   mounted () {
     this.init()
   },
   methods: {
     init () {
-      const map = L.map('map', {
+      this.initMap()
+      this.initMarkers()
+    },
+    initMap () {
+      this.map = L.map('map', {
         center: center,
         zoom: 13,
         maxZoom: 18,
@@ -33,26 +61,23 @@ export default {
           minZoom: 1,
           ak: '您申请的key值'
         })
-        .addTo(map)
+        .addTo(this.map)
 
-      // marker icon自定义
-      const Icon = L.Icon.extend({
-        options: {
-          iconUrl: require('@/assets/gis/monitor.png'),
-          iconSize: [36, 39],
-          iconAnchor: [18, -5]
-        }
+      // 地图被点击时，marker恢复原style
+      this.map.addEventListener('click', () => {
+        this.curMarker && this.curMarker.setIcon(icon)
       })
-      const icon = new Icon()
-
-      // 造100个点
+    },
+    initMarkers () {
+      // 造100个随机点
       const arr = new Array(100)
       const markers = []
       arr.fill(1).forEach((item, i) => {
         let marker = new L.Marker(
           i % 2 === 0 ? [center[0] + i * 0.01 * Math.random(), center[1] + i * 0.01 * Math.random()] : [center[0] - i * 0.01 * Math.random(), center[1] - i * 0.01 * Math.random()],
           {
-            icon: icon
+            icon: icon,
+            id: i
           }
         )
         const content = `
@@ -60,14 +85,28 @@ export default {
           我是成都<br/>
           编号：${i + 1}
         `
-        marker.bindPopup(content)
+        marker.bindPopup(content, {
+          minWidth: 200,
+          maxWidth: 500,
+          maxHeight: 200
+        })
+        // 点击修改marker的状态
         marker.on('click', e => {
-          console.log('marker被点击了')
+          console.log(`marker被点击了，编号${marker.options.id}`)
+
+          let curMarker = this.curMarker
+          if (curMarker !== marker) {
+            curMarker && curMarker.setIcon(icon)
+            marker.setIcon(iconActive)
+            this.curMarker = marker
+          } else {
+            marker.openPopup()
+          }
         })
         markers.push(marker)
       })
       // 批量打点
-      L.layerGroup(markers).addTo(map)
+      L.layerGroup(markers).addTo(this.map)
     }
   }
 }
